750f0ddac2d0d6e27d73b4c6c7af18dfeb20f23c
Derek Maffett <derekgmaffett@gmail.com>
2014-07-21 02:48:41 -0700

    Initial commit

---
+++ app/models/task.rb
@@ -0,0 +1,20 @@
+class Task < ActiveRecord::Base	
+
+	def mark_completed(date = nil)
+		self.completed_at = (date || Time.current)
+	end
+
+	def complete?
+		!completed_at.nil?
+	end
+
+	def counts_toward_velocity?
+		return false unless complete?
+		completed_at > Project.velocity_length_in_days.days.ago
+	end
+
+	def points_toward_velocity
+		if counts_toward_velocity? then size else 0 end
+	end
+
+end
